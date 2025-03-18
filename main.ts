8901d81fca4d14f1d15d6e02af7359cebb68315d
Riberto12 <132398116+Riberto12@users.noreply.github.com>
2025-03-18 14:44:57 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -134,6 +134,4 @@ const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-const port = Number(Deno.env.get("PORT")) || 8000;
-console.log(`Servidor rodando na porta ${port}`);
-await app.listen({ port });
+await app.listen();
